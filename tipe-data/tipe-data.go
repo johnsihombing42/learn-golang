18210cf891fb04624bb569f4cf2f19d3767355de
johnsihombing42 <johnsihombing42@gmail.com>
2022-09-20 21:47:33 +0700

    update golang

---
+++ tipe-data/tipe-data.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//tipe data number
+    fmt.Println("Satu :",1)
+	fmt.Println("Dua :",2)
+
+	//tipe data boolean
+	fmt.Println("Benar :",true)
+	fmt.Println("Salah : ",false)
+
+	//tipe data string
+	
+}
