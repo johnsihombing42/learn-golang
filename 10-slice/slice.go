dc86dcc14d64bc1ddd82b6e5d4bbfa78c30b6f93
johnsihombing42 <johnsihombing42@gmail.com>
2022-10-11 23:16:53 +0700

    slice

---
+++ 10-slice/slice.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var months = [...]string{
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember",
+	}
+
+	fmt.Println("Slice 1")
+	var slice1 = months[4:7]
+	fmt.Println(slice1)
+	fmt.Println(len(slice1))
+	fmt.Println(cap(slice1))
+
+	fmt.Println("Slice 2")
+	var slice2=months[10:]
+	fmt.Println(slice2)
+
+	fmt.Println("Append Slice 2")
+	var slice3=append(slice2,"John")
+	fmt.Println(slice3)
+
+}
--- 9-array/array.go
+++ 9-array/array.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	var names [3]string
+	names[0] = "John"
+	names[1] = "Tri"
+	names[2] = "Putra"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+	fmt.Println(len(names))
+
+	//array
+	var values=[3]int{
+		80,
+		90,
+		100,
+	}
+	fmt.Println(len(values))
+	fmt.Println(values)
 }
