a6ea74831bdf22218afcbb58a4c47b9cb285bda1
johnsihombing42 <johnsihombing42@gmail.com>
2022-10-11 22:48:36 +0700

    add math operation

---
+++ 6-math-operation/math-operation.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a=10
+	var b=20
+	var tambah=a+b
+	var kurang=a-b
+	var kali=a*b
+	var bagi=a/b
+	fmt.Println(tambah)
+	fmt.Println(kurang)
+	fmt.Println(kali)
+	fmt.Println(bagi)
+
+	var i=10
+	i=i+15
+	fmt.Println(i)
+}
