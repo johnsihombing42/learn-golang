1be66796b90bc955e54b8a749813e67ed9576dba
johnsihombing42 <johnsihombing42@gmail.com>
2022-10-11 23:38:03 +0700

    for loop

---
+++ 13-for-loop/for-loop.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//perulanga for biasa
+	// counter:=1
+	// for counter<=10{
+	// 	fmt.Println("Perulangan ke",counter)
+	// 	counter++
+	// }
+
+	for counter := 1; counter <= 10; counter++ {
+		fmt.Println("Perulangan ke", counter)
+	}
+
+}
