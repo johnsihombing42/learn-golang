8d8b7573b60d55280b97b7a03ddf13b46d215c90
johnsihombing42 <johnsihombing42@gmail.com>
2022-10-11 22:51:36 +0700

    add comparation

---
+++ comparation-operator.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "John"
+	var name2 = "John"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+	fmt.Println()
+	//comparasi bilangan
+	var value1=100
+	var value2=300
+
+	fmt.Println(value1<value2)
+	fmt.Println(value1<value2)
+	fmt.Println(value1!=value2)
+	fmt.Println(value1==value2)
+}
