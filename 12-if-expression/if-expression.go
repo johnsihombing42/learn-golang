7621bea2d3e585a2fcb6e3ce63596bb3897acdb2
johnsihombing42 <johnsihombing42@gmail.com>
2023-01-30 19:15:00 +0700

    update

---
+++ 12-if-expression/if-expression.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "John Sihombing1"
+
+	if name == "John Sihombing" {
+		fmt.Println("Hello John")
+	} else if name== "Joko"{
+		fmt.Println("Ini Joko")
+	} else{
+		fmt.Println("Ini bukan John")
+	}
+	
+}
