651f44c2d48149dee04b4aba61725fd8ce27d4d3
johnsihombing42 <johnsihombing42@gmail.com>
2023-02-03 23:54:04 +0700

    learn pointer

--- 21-pointer/pointer-basic.go
+++ 21-pointer/pointer-basic.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// numberA := 5
-	// numberB := &numberA
-	// fmt.Println(numberA)
-	// fmt.Println(numberB)
-	// fmt.Println(*numberB)
+	numberA := 5
+	numberB := &numberA
+	fmt.Println(numberA)
+	fmt.Println(numberB)
+	fmt.Println(*numberB)
 
-	// *numberB=10
-	// fmt.Println(numberA)
-	// fmt.Println(*numberB)
+	*numberB=10
+	fmt.Println(numberA)
+	fmt.Println(*numberB)
+	fmt.Println("........")
 
 	// var numberA int = 5
 	// var numberB *int = &numberA
