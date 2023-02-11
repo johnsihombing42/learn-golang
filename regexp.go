1c8990a2bff3d91994445a526b43ac5d2deee908
johnsihombing42 <johnsihombing42@gmail.com>
2023-02-11 17:35:21 +0700

    add regexp

---
+++ regexp.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+    var text = "banana burger soup"
+    var regex, err = regexp.Compile(`[a-z]+`)
+
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+
+    var res1 = regex.FindAllString(text, 2)
+    fmt.Printf("%#v \n", res1)
+
+
+    var res2 = regex.FindAllString(text, -1)
+    fmt.Printf("%#v \n", res2)
+}
