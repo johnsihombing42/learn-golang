56031656d732ab9f55b668bd60982ad207721fc7
johnsihombing42 <johnsihombing42@gmail.com>
2023-02-27 21:55:55 +0700

    add

---
+++ 26-web-server/web-server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "apa kabar!")
+}
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "halo!")
+    })
+
+    http.HandleFunc("/index", index)
+
+    fmt.Println("starting web server at http://localhost:8080/")
+    http.ListenAndServe(":8080", nil)
+}
